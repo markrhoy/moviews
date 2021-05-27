470b694983e534de9511850608bf8dd18b84ebb3
Mark Rhoy <markrhoy.sabal@mynt.xyz>
2021-05-27 11:49:02 +0800

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,89 @@
-import { Badge, Col, Row, Typography, List, Card, Image, Space, Rate, Pagination } from 'antd';
+import { Input, Form, Badge, Col, Row, Typography, List, Card, Image, Space, Rate, Pagination, Carousel, Affix, Button } from 'antd';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import {
+  SmileOutlined,
+  SearchOutlined
+} from '@ant-design/icons';
 import Movie from './components/Movie';
 
-const {Text} = Typography
+const {Text, Link} = Typography
+const {Search} = Input
 
 const BASE_URL = "https://api.themoviedb.org/3"
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
 
 const App = () => {
   const [trendingMovies, setTrendingMovies]  = useState({})
+  const [pageNumber, setPageNumber] = useState(1)
 
   useEffect(() => {
+    setTrendingMovies({})
     axios.get(
       BASE_URL + '/trending/movie/day', 
-      {params: {'api_key': process.env.REACT_APP_KEY}}
+      {params: {
+        'api_key': process.env.REACT_APP_KEY,
+        'page':pageNumber}}
     ).then( res => {
       setTrendingMovies(res.data)
     })
-  }, [])
 
-  console.log(trendingMovies)
+    
+
+  }, [pageNumber])
+
+  const handleOnchange = page => {
+    setPageNumber(page)
+  }
+
+  const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
   return (
     <div className="app">
-      <header>
+      <nav>
         <Row justify="center" className="nav" align="middle">
           <Col span={18}>
-            <Text>Moviews</Text>
+            <Link href="/" >
+              <Text type="warning" strong style={{
+                fontSize: '25px',
+                textTransform: 'uppercase'
+              }}> M<SmileOutlined />views</Text>
+            </Link>
           </Col>
         </Row>
+      </nav>
+      <header>
+          <Row justify="center" align="middle" className="search-bar" gutter={6}>
+            <Col span={14}>
+            <Search
+              placeholder="Type movie title ..."
+              enterButton="Search"
+              size="large"
+              
+            />
+            </Col>
+          </Row>
       </header>
+      
       <main className="main-content">
         <Row justify="center">
           <Col span={18} >
             <List
               grid={{
                 gutter: 16,
-                column: 4
+                xs: 1,
+                sm:2,
+                md:3,
+                column:4
+                
               }}
               dataSource={trendingMovies.results}
               renderItem={ item => (
@@ -45,10 +92,11 @@ const App = () => {
                 </List.Item>
               )}
             />
-
             <Row justify="center">
               <Col>
                 <Pagination
+                defaultCurrent={pageNumber}
+                onChange={handleOnchange}
                 total={trendingMovies.total_pages}
                 responsive={true}/>
               </Col>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,8 @@
-import {Card, Image, Space, Rate, Typography, Drawer,Divider } from 'antd';
+import { Skeleton,Card, Image, Space, Rate, Typography, Drawer,Divider, Tag } from 'antd';
 import {
     CalendarOutlined,
     TeamOutlined,
-    EyeOutlined
+    VideoCameraOutlined,
 } from '@ant-design/icons';
 import '../App.css';
 import axios from 'axios';
@@ -13,11 +13,14 @@ const {Meta} = Card
 
 const BASE_URL = "https://api.themoviedb.org/3"
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+const FALLBACK_IMAGE_URL = "https://www.virginmediastore.com/media/tile-placeholder-poster.2769cb5f.png"
 
 const Movie = props => {
     const movieId = props.id
     const [details, setDetails] = useState({})
     const [visible, setVisible] = useState(false);
+    const [isLoading, setIsLoading] = useState(true)
+   
 
     useEffect(() => {
         axios.get(
@@ -25,8 +28,10 @@ const Movie = props => {
             { params: { 'api_key': process.env.REACT_APP_KEY } }
         ).then(res => {
             setDetails(res.data)
+            setIsLoading(false)
         })
-    }, [])
+
+    }, [movieId])
 
     const showDrawer = () => {
         setVisible(true);
@@ -35,23 +40,31 @@ const Movie = props => {
     const onClose = () => {
         setVisible(false);
     };
+
+    
     
     return (
         <div>
             <Card
+                loading={isLoading}
                 bordered={false}
                 cover={
                     <Image
+                        placeholder={
+                            <Image
+                                width={"auto"}
+                                src={FALLBACK_IMAGE_URL}
+                            />
+                        }
                         width={'auto'}
                         alt={details.title}
                         src={BASE_IMAGE_URL + details.poster_path}
+                        fallback={BASE_IMAGE_URL + details.poster_path}
                     />
-                }
-                
-            >
+                }>
                 <Meta
                     title={
-                        <Text className="card-title" onClick={showDrawer}>{details.original_title}</Text>
+                        <Text ellipsis={false} className="card-title" onClick={showDrawer}>{details.original_title}</Text>
                     }
                     description={
                         <div>
@@ -91,6 +104,7 @@ const Movie = props => {
                 <Space direction="vertical">
                     <Image 
                         width={'auto'}
+                        placeholder={true}
                         alt={details.title}
                         src={BASE_IMAGE_URL + details.poster_path}
                     />
@@ -108,10 +122,8 @@ const Movie = props => {
                     <Divider type="vertical" />
                     <Rate disabled value={details.vote_average / 2} allowHalf={true} />
                     </Space>
-
                     <Paragraph>{details.overview}</Paragraph>
                     
-
                 </Space>
                 
             </Drawer>
